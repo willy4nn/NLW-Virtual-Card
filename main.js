7b794ff233f735e15864b2fea7f1fdfa4ac57bad
Willyan Nascimento <101363317+willy4nn@users.noreply.github.com>
2022-05-23 15:23:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+const linksSocialMedia = {
+  github : 'willy4nn',
+  youtube : 'RocketSeat',
+  facebook : 'rocketseat',
+  instagram : 'rocketseat_oficial',
+  twitter : 'rocketseat'
+}
+
+function changeSocialMediaLinks(){
+  for (let li of socialLinks.children){
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos(){
+ const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    userName.textContent = data.name
+    userBio.textContent = data.bio
+    userLink.href = data.html_url
+    userImage.src = data.avatar_url
+    userLogin.textContent = data.login
+  })
+}
+
+
+
+getGitHubProfileInfos()
+
+
+
